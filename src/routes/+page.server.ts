ca8d1a9cdb4880690ff23adf39eba340fa86a9f9
Aron Kylebäck <aron@kyleback.com>
2026-08-22 18:39:09 +0200

    Fix return value from page.server.ts

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,7 +1,7 @@
 import type { PageServerLoad } from './$types';
 import { users } from '../db/schema.ts';
 import { eq } from 'drizzle-orm';
-import { json } from '@sveltejs/kit';
+import { error, json } from '@sveltejs/kit';
 
 export const load: PageServerLoad = async ({ platform, locals }) => {
 	const apiKey = platform?.env?.steam_webapi;
@@ -10,13 +10,12 @@ export const load: PageServerLoad = async ({ platform, locals }) => {
 	const res = await fetch(
 		`https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${apiKey}&steamids=${acceptedUsers.map((u) => u.steamId).join(',')}`
 	);
-	if (!res.ok) return json({ error: 'Steam API request failed' }, { status: 502 });
+	if (!res.ok) error(502, 'Steam API request failed');
 
 	const data = await res.json();
 	// @ts-expect-error Too lazy to create interface
 	const rawPlayers: Player[] = data.response.players;
-	if (!rawPlayers || rawPlayers.length == 0)
-		return json({ error: 'No such profile' }, { status: 404 });
+	if (!rawPlayers || rawPlayers.length == 0) error(404, 'No such profile');
 
 	const players = rawPlayers.map((player) => {
 		const match = acceptedUsers.find((u) => u.steamId === player.steamid);
